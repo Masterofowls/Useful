c5da8adf6f6b70ddc83e30053f3cca9f30f828d8
Masterofowls <danvlad2015@gmail.com>
2025-01-24 06:05:25 +0300

    3

--- useful.jsx
+++ useful.jsx
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 
 const App = () => (
   <div>
-    <h1>Useful Web Examples</h1>
-    <h2>
-    <ul>
-      <li> wget --wait=2 --level=inf --limit-rate=20K --recursive --page-requisites --user-agent=Mozilla --no-parent --convert-links --adjust-extension --no-clobber -e robots=off https://spaces.w3schools.com</li>
-    </ul>
-    </h2>
+    <div>
+      <h1>Useful</h1>
+      <h2>Wget</h2>
+      <ul>
+        <li> wget --wait=2 --level=inf --limit-rate=20K --recursive --page-requisites --user-agent=Mozilla --no-parent --convert-links --adjust-extension --no-clobber -e robots=off https://spaces.w3schools.com</li>
+      </ul>
+    </div>
+    <div>
+      <h2>Curl</h2>
+      <ul>
+        <li></li>
+      </ul>
+    </div>
   </div>
 );
 
